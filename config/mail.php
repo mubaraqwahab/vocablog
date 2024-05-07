924b099dc3932d60ec1e1acbd6cf0664b359946a
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-05-07 23:13:00 +0100

    ...mail config for fly

--- config/mail.php
+++ config/mail.php
@@ -96,6 +96,6 @@ return [
 
     "from" => [
         "address" => env("MAIL_FROM_ADDRESS", "hello@example.com"),
-        "name" => env("MAIL_FROM_NAME", "Example"),
+        "name" => env("MAIL_FROM_NAME", env("APP_NAME")),
     ],
 ];
