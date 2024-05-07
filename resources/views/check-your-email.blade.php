30c9b1655ffd2a4af06a2788091252e5f91a7264
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-05-07 23:18:55 +0100

    fix "account" typo

--- resources/views/check-your-email.blade.php
+++ resources/views/check-your-email.blade.php
@@ -2,6 +2,6 @@
   <h1 class="PageHeading">Check your email</h1>
   <p>
     You'll receive a login link soon. Click the link,
-    and you'll be able to access your count.
+    and you'll be able to access your account.
   </p>
 </x-layout>
