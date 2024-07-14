0ebefca1e7b752d067ea7bd913c4f8a0460dbbcf
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-07-14 10:21:04 +0100

    return weekly digest schedule to 12pm every sunday

--- routes/console.php
+++ routes/console.php
@@ -2,35 +2,34 @@
 
 use App\Mail\WeeklyDigest;
 use App\Models\User;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Log;
 
 Artisan::command("inspire", function () {
     $this->comment(Inspiring::quote());
 })->purpose("Display an inspiring quote");
 
 Artisan::command("app:send-digest", function () {
-    Log::info("Running weekly digest");
+    $termsLoader = function ($query) {
+        $query
+            ->withWhereHas("definitions", function ($query) {
+                $query->whereBetween("created_at", [now()->addWeeks(-1), now()]);
+            })
+            ->orderBy("name", "asc");
+    };
+
     $users = User::query()
         ->where("weekly_digest_enabled", true)
-        ->withWhereHas("terms", function ($query) {
-            $query
-                ->withWhereHas("definitions", function ($query) {
-                    $query->whereBetween("created_at", [now()->addWeeks(-1), now()]);
-                })
-                ->orderBy("name", "asc");
-        })
+        ->withWhereHas("terms", $termsLoader)
         ->orderBy("id", "asc")
         ->paginate(10);
 
     foreach ($users as $user) {
-        Log::info("Sending digest to " . $user->email);
         Mail::to($user)->sendNow(new WeeklyDigest($user));
     }
 })
     ->purpose("Send a weekly digest to all users")
-    // ->weeklyOn(Schedule::SUNDAY, "21:10")
-    ->everyFiveMinutes();
-// ->timezone("Africa/Lagos");
+    ->weeklyOn(Schedule::SUNDAY, "12:00")
+    ->timezone("Africa/Lagos");
